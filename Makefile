32ed530bae7912f01f9480f8f77ca354bbbeab06
Michael Benford <michael@michaelbenford.net>
2021-08-20 00:46:38 -0300

    Enable selecting what modules to build

--- Makefile
+++ Makefile
@@ -1,20 +1,30 @@
 .PHONY: flash, image
 
-IMAGE_NAME = ap2-qmk:local
-MODEL = c18
-KEYMAP = default
-FLASH = true
+SRC_DIR=$(PWD)
+IMAGE_NAME=ap2-qmk:local
+MODEL=c18
+KEYMAP=default
+MODULES=all
+FLASH=no
+
+ifeq ($(FLASH),yes)
+	PRIVILEGED=true
+else
+	PRIVILEGED=false
+endif
 
 all:
-	@if [ -z "$(DATA_DIR)" ]; then echo "error: DATA_DIR is required"; exit 1; fi
 	@rm -rf ./build
 	@mkdir build
 	docker run \
 		--rm \
-		--privileged=$(FLASH) \
-		-e MODEL=$(MODEL) -e KEYMAP=$(KEYMAP) -e FLASH=$(FLASH) \
-		-v $(DATA_DIR):/data -v $(PWD)/build:/build -v /dev:/dev \
+		--privileged=$(PRIVILEGED) \
+		-e MODEL=$(MODEL) -e KEYMAP=$(KEYMAP) -e MODULES=$(MODULES) -e FLASH=$(FLASH) \
+		-v $(SRC_DIR):/src -v $(PWD)/build:/build -v /dev:/dev \
 		$(IMAGE_NAME) ./build.sh
+
+flash: FLASH=yes
+flash: all
 
 image:
 	docker build -t $(IMAGE_NAME) .
--- build.sh
+++ build.sh
@@ -3,27 +3,30 @@ set -e
 
 : ${MODEL:=c18}
 : ${KEYMAP:=default}
-: ${FLASH:=true}
+: ${MODULES:=all}
+: ${FLASH:=yes}
 
 KEYMAP_FILENAME="ap2-${MODEL}-${KEYMAP}.bin"
 SHINE_FILENAME="ap2-${MODEL}-shine.bin"
 
-mkdir -p ~/qmk/keyboards/annepro2/keymaps/$KEYMAP
-cp /data/keymaps/$KEYMAP/* ~/qmk/keyboards/annepro2/keymaps/$KEYMAP
-cd ~/qmk
-ALLOW_WARNINGS=yes make annepro2/${MODEL,,}:$KEYMAP
+if [[ $MODULES =~ (all|keymap) ]]; then
+	[[ -d "/src/keymaps/$KEYMAP" ]] && cp -r /src/keymaps/$KEYMAP ~/qmk/keyboards/annepro2/keymaps
+	cd ~/qmk
+	ALLOW_WARNINGS=yes make annepro2/${MODEL,,}:$KEYMAP
+	cp ~/qmk/.build/annepro2_c18_$KEYMAP.bin /build/$KEYMAP_FILENAME
+fi
 
-cp /data/shine/* ~/shine/source
-cd ~/shine
-make ${MODEL^^}
+if [[ $MODULES =~ (all|shine) ]]; then
+	[[ -d "/src/shine" ]] && cp /src/shine/* ~/shine/source
+	cd ~/shine
+	make ${MODEL^^}
+	cp ~/shine/build/annepro2-shine-C18.bin /build/$SHINE_FILENAME
+fi
 
-cp ~/qmk/.build/annepro2_c18_$KEYMAP.bin /build/$KEYMAP_FILENAME
-cp ~/shine/build/annepro2-shine-C18.bin /build/$SHINE_FILENAME
-
-if [ $FLASH == "true" ]; then
+if [ $FLASH == yes ]; then
 	echo "Flashing in 10 seconds. Put the keyboard into IAP mode!"
 	sleep 10
 	cd ~/tools/target/release/
-	./annepro2_tools /build/$KEYMAP_FILENAME
-	./annepro2_tools --boot -t led /build/$SHINE_FILENAME
+	[[ -f "/build/$KEYMAP_FILENAME" ]] && ./annepro2_tools /build/$KEYMAP_FILENAME
+	[[ -f "/build/$SHINE_FILENAME" ]] && ./annepro2_tools --boot -t led /build/$SHINE_FILENAME
 fi
