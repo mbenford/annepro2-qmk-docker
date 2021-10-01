12520f2a01f389c1c335211453bfce541ce7c854
Michael Benford <michael@michaelbenford.net>
2021-09-30 23:44:34 -0300

    Fix variable substitution

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ if [[ $MODULES =~ (all|keymap) ]]; then
 	[[ -d "/src/keymaps/$KEYMAP" ]] && cp -r /src/keymaps/$KEYMAP ~/qmk/keyboards/annepro2/keymaps
 	cd ~/qmk
 	ALLOW_WARNINGS=yes make annepro2/${MODEL,,}:$KEYMAP
-	cp ~/qmk/.build/annepro2_$MODEL_$KEYMAP.bin /build/$keymap_filename
+	cp ~/qmk/.build/annepro2_${MODEL}_${KEYMAP}.bin /build/$keymap_filename
 	boot_at="keymap"
 fi
 
@@ -21,7 +21,7 @@ if [[ $MODULES =~ (all|shine) ]]; then
 	[[ -d "/src/shine" ]] && cp /src/shine/* ~/shine/source
 	cd ~/shine
 	make ${MODEL^^}
-	cp ~/shine/build/annepro2-shine-$MODEL.bin /build/$shine_filename
+	cp ~/shine/build/annepro2-shine-${MODEL^^}.bin /build/$shine_filename
 	boot_at="shine"
 fi
 
